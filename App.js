390748b7b1ff411ff24d6d26aa6e2e7b5299d267
Caspian Thackeray <cthackt@gmail.com>
2024-01-29 23:43:20 -0800

    first commit

--- App.js
+++ App.js
@@ -1,14 +1,70 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import LotsOfStyles from './components/LotsOfStyles';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Flexample from './components/Flexample';
+import PressMe from './components/Pressable';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      {/* <View style={styles.container}>
+        <StatusBar style="auto" />
+        <Text>App</Text>
+        <LotsOfStyles />
+        <Button onPress={handleTouch} title="Greet"/>
+      </View> */}
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'WATER BUDDIES!!!' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+        <Stack.Screen name="Flexample" component={Flexample} />
+        <Stack.Screen name="PressMe" component={PressMe} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <>
+      <Button
+        title="Go to Jane's profile"
+        onPress={() =>
+          navigation.navigate('Profile', {name: 'Jane'})
+        }
+      />
+      <Button
+        title="Go to Cas's profile"
+        onPress={() =>
+          navigation.navigate('Profile', {name: 'Cas'})
+        }
+      />
+      <Button
+        title="Go to Flexample"
+        onPress={() =>
+          navigation.navigate('Flexample')
+        }
+      />
+      <Button
+        title="Go to Pressable example"
+        onPress={() =>
+          navigation.navigate('PressMe')
+        }
+      />
+    </>
+  );
+};
+
+const ProfileScreen = ({navigation, route}) => {
+  return <Text>This is {route.params.name}'s profile</Text>;
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +74,8 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+const handleTouch = () => {
+  alert("Hello")
+}
+
