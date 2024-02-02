98a382871a8a1adcce11d105ec9805f9a5685ce5
Caspian Thackeray <cthackt@gmail.com>
2024-02-01 19:54:27 -0800

    small change

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,16 +1,13 @@
 import { Stack } from "expo-router"
-import { createContext, useContext, useState } from "react"
+import { createContext, useContext } from "react"
 
 export default RootLayout = () => {
 
    const AuthContext = createContext()
-   const [token, setToken] = useState('')
 
    return (
-      <AuthContext value={{ token, setToken}}>
-         <Stack>
-            <Stack.Screen name="index" options={{ title: "Home" }}></Stack.Screen>
-         </Stack>
-      </AuthContext>
+      <Stack>
+         <Stack.Screen name="index" options={{ title: "Home" }}></Stack.Screen>
+      </Stack>
    )
 }
